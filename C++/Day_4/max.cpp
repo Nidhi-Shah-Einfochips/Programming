55b0364e4372edb5e5adc8b106301090018f20ce
NidhiShah-NS <98095643+NidhiShah-NS@users.noreply.github.com>
2022-02-23 15:38:51 +0530

    Add files via upload

---
+++ C++/Day_4/max.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int& max(int &,int &);
+int main()
+{
+	int a=5,b=6,ans;
+	ans=max(a,b);
+	cout<<"Maximum="<<ans<<endl;
+}
+int& max(int &x, int&y)
+{
+	if(x>y)
+		return ++x;
+	else
+		return ++y;
+}
