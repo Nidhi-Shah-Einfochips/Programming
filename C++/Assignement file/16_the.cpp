87f36d0dffd563f045450874f1e1815ffcab8c21
NidhiShah-NS <98095643+NidhiShah-NS@users.noreply.github.com>
2022-03-21 11:19:04 +0530

    Add files via upload

---
+++ C++/Assignement file/16_the.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+  fstream fin("16_file.txt");
+  int count = 0;
+  char ch[20];
+  char c[5]="the";
+  char c1[5]="The";
+  while(fin)
+    {
+      fin>>ch;
+      if(strcmp(ch,c)==0||strcmp(ch,c1)==0)
+      {
+	      count++;
+      }
+    }
+
+  cout << count << endl;
+	fin.close();  
+
+  return 0;
+}
+
